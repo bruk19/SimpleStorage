d372e11b31b52007943f1d99c104a921f64ebfe5
Bruk Teshome <brukteshome21@gmail.com>
2024-01-26 11:46:38 +0300

    Add env plugin to secure wallet

--- deploy.js
+++ deploy.js
@@ -1,21 +1,28 @@
-const ethers = require('ethers');
+const { ethers, JsonRpcProvider } = require('ethers');
 const fs = require('fs-extra');
+require('dotenv').config();
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider('http://0.0.0.0:8545');
-  const wallet = new ethers.Wallet(
-    '',
-    provider
-  );
+  const provider = new JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const binary = fs.readFileSync(
     './SimpleStorage_sol_SimpleStorage.bin',
     'utf8'
   );
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
+  console.log('Deploying, please wait...');
   const contract = await contractFactory.deploy();
   console.log(contract);
+  // await contract.deployTransaction.wait(1);
+
+  // // console.log(`Contract Address: ${contract.address}`)
+  // const currentFavoriteNumber = await contract.retrieve();
+  // console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+  // const transactionResponse = await contract.store('7');
+  // const transactionReceipt = await transactionResponse.wait(1);
+  // const updatedFavoriteNumber = await contract.retrieve();
+  // console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
 }
 
 main()
