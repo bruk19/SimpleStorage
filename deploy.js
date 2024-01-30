5023ddbe49ce9772b8be6c5d06cc2c247ca2cf56
Bruk Teshome <brukteshome21@gmail.com>
2024-01-30 18:32:16 +0300

    update transctional Response

--- deploy.js
+++ deploy.js
@@ -13,10 +13,8 @@ async function main() {
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log('Deploying, please wait...');
   const contract = await contractFactory.deploy();
-  console.log(contract);
   await contract.deployTransaction.wait(1);
-
-  console.log(`Contract Address: ${contract.address}`)
+  console.log(`Contract Address: ${contract.address}`);
   const currentFavoriteNumber = await contract.retrieve();
   console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
   const transactionResponse = await contract.store('7');
